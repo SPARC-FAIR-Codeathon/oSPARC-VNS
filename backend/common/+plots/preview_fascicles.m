ad1033821cd845ea93597b639b58ae5acdd65145
Calvin Eiber <calvin.eiber@unimelb.edu.au>
2021-07-16 07:51:42 +1000

    working on getting human vagus sample in

--- backend/common/+plots/preview_fascicles.m
+++ backend/common/+plots/preview_fascicles.m
@@ -2,6 +2,9 @@
 
 function s = preview_fascicles (varargin)
 % preview display for mesh.insert_gmsh_fascicles
+% TODO: add visualisation for 3d fascicles
+
+named = @(v) strncmpi(v,varargin,length(v)); 
 
 s = mesh.insert_gmsh_fascicles(varargin{:},'-info');
 
@@ -20,29 +23,25 @@ cla reset, hold on
 
 nF = size(s.coeffs,3);
 C = lines(max(nF,7)); G = @(v) [v v v]/10;
-
 do_warn = []; 
 
 for ii = 1:nF
     plot(s.coeffs(1,:,ii), s.coeffs(2,:,ii),'o','Color',C(ii,:),'MarkerSize',4)
-    plot(s.outline(:,1,ii), s.outline(:,2,ii),'-','Color',C(ii,:))   
-    if nF > 7, 
-        text(mean(s.outline(2:end,1,ii)), ...
-             mean(s.outline(2:end,2,ii)),sprintf('#%d',ii),'Color',C(ii,:))
-    end
+    plot(s.outline(:,1,ii), s.outline(:,2,ii),'-','Color',C(ii,:))    
     
     if any(s.outline(:,2,ii) < 0), do_warn = [do_warn ii]; end %#ok<AGROW>
 end
 
 axis equal, grid on, tools.tidyPlot, ax = axis; 
 
+if any(named('-no-w')), return, end
+
 fill(ax([1 2 2 1 1]),[0 0 ax([3 3]) 0],G(3),'FaceAlpha',0.3,'EdgeColor',G(3),'LineWidth',1.1)
 
 if ~isempty(do_warn)
-    
     do_warn = sprintf(',#%d',do_warn);
     text(mean(xlim),mean(ylim),sprintf('Negative Y in fascicles: %s', ...
-               do_warn(2:end)),'Color','r','FontSize',14,'Horiz','center')
+                               do_warn(2:end)),'Color','r','FontSize',14)
 end
 
 if nargout == 0, clear, end
--- backend/module-mesher/test.m
+++ backend/module-mesher/test.m
@@ -1,18 +1,9 @@
 
 if exist('compile','dir')
-    
-    disp('Testing COMPILED binaries')
-    
-    ! ./compile/for_testing/nerve_mesher "./input/demo/array.json" "" "input/demo/nerve-script.json"
-    
-    
-    return
+  disp('Testing COMPILED binaries')
+  ! ./compile/for_testing/nerve_mesher "./input/demo/array.json" "" "input/demo/nerve-script.json"
+  return
 end
-    
-    
-
-
-
 
 %% EXAMPLE (1): planar-array
 
@@ -34,4 +25,24 @@ nerve_mesh(tools.file('input~/demo/payne2019-cuff.json'), '', ...
            tools.file('input~/demo/payne2019-nerve.json'))
 
        
-       
+%% EXAMPLE (3)
+
+human_vagus = tools.parse_json( tools.file('get','*.json') );
+mesh.insert_gmsh_fascicles('-setup',human_vagus)
+nerve = plots.preview_fascicles;
+
+array = tools.parse_json('./input/demo/payne2019-cuff.json');
+
+hold on, plot([1 -1 -1 1 1]*array.array.carrier.cuff_IDx/0.002, ...
+              [1 1 -1 -1 1]*array.array.carrier.cuff_IDy/0.002, 'k-', ...
+              'LineWidth', 1.5)
+          
+
+%%
+
+tools.file('root',pwd)
+nerve_mesh(tools.file('input~/demo/payne2019-cuff.json'), ...
+                        human_vagus_sample)
+
+
+           
