a4ea840fbc365736654a9c1939e61abcfde64887
Calvin Eiber <calvin.eiber@unimelb.edu.au>
2021-07-22 10:03:57 +1000

    organising /app/ better

---
+++ frontend/app/generate_user_files.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 22 02:35:52 2021
+
+@author: Calvin
+"""
+
+import json
+
+
+
+def mk_nerve(rot=0,dx=0,dy=0,sz=12):
+    
+    s = {"nerve":{"xRotate":rot, 
+                  "xMove":[dx,dy],
+                  "zRange":sz,
+                  "meshList":["Fascicle","Epineurium"],
+                  "MeshLengthFascicle":0.1               }}
+
+    return json.dumps(s)
+
+
+
+
+def mk_mesh(rot=0,dx=0,dy=0,sz=12):
+    
+    s = {"nerve":{"xRotate":rot, 
+                  "xMove":[dx,dy],
+                  "zRange":sz,
+                  "meshList":["Fascicle","Epineurium"],
+                  "MeshLengthFascicle":0.1               }}
+
+    return json.dumps(s)
